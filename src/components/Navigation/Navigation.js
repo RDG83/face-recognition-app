308a8b35ae44c145e4ed872e10bd277ffe060fd0
RDG83 <rdgrijs@hotmail.com>
2020-04-08 14:22:39 +0200

    removed default files and made nav

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <nav className="nav">
+            <p>Sign Out</p>
+            <p>About</p>
+            <p>Register</p>
+        </nav>
+    );
+};
+
+export default Navigation;
