5282a794b2f85adfcff75f8a7207f037ca0bfbfa
RDG83 <rdgrijs@hotmail.com>
2020-04-09 11:27:37 +0200

    add image form input

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
-import 'tachyons';
-import Logo from './components/Logo/Logo'
+import "tachyons";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 
 function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      {<Logo />
-      /*
-      <ImageLinkForm />
-      <FaceRecognition />*/}
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navigation />
+            <Logo />
+            <ImageLinkForm />
+            {/*<FaceRecognition />*/}
+        </div>
+    );
 }
 
 export default App;
