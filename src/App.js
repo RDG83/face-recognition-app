6d531a92c76ac89b5094cb2592101debbcf89d0a
RDG83 <rdgrijs@hotmail.com>
2020-04-09 10:30:42 +0200

    add logo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navigation from "./components/Navigation/Navigation";
 import 'tachyons';
+import Logo from './components/Logo/Logo'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
+      {<Logo />
+      /*
       <ImageLinkForm />
       <FaceRecognition />*/}
     </div>
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Tilt from "react-tilt";
+import "./Logo.css";
+import face from "./face.png";
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt
+                className="Tilt br2 shadow-2"
+                options={{ max: 35 }}
+                style={{ height: 150, width: 150 }}
+            >
+                <div className="Tilt-inner pa1">
+                    {" "}
+                    <img className="mt2" src={face} alt="face" />{" "}
+                </div>
+            </Tilt>{" "}
+        </div>
+    );
+};
+
+export default Logo;
