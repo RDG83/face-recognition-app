6810e126b685551022069016545dd26cf2585bab
RDG83 <rdgrijs@hotmail.com>
2020-04-09 12:14:44 +0200

    add particles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import Navigation from "./components/Navigation/Navigation";
 import "tachyons";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
-import Rank from './components/Rank/Rank';
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
 
+const particleOptions = {
+    particles: {
+      number: {
+        value: 150
+      },
+        line_linked: {
+            shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5,
+            },
+        },
+    },
+};
 function App() {
     return (
         <div className="App">
+            <Particles className="particles" params={particleOptions} />
             <Navigation />
             <Logo />
             <Rank />
