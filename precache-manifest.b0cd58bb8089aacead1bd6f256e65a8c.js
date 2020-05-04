53ce8c867e9c527ca0bd111bc622cb74af8eba66
Robert de Grijs <rdgrijs@hotmail.com>
2020-05-04 18:34:38 +0200

    Updates

---
+++ precache-manifest.b0cd58bb8089aacead1bd6f256e65a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f41071b1c48bbd1ceb279f2f0339a40",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "683796fe1fcf14693fbb",
+    "url": "/face-recognition-app/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7f800279add3076d3e4f",
+    "url": "/face-recognition-app/static/css/main.4607a71b.chunk.css"
+  },
+  {
+    "revision": "683796fe1fcf14693fbb",
+    "url": "/face-recognition-app/static/js/2.c74621b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-recognition-app/static/js/2.c74621b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f800279add3076d3e4f",
+    "url": "/face-recognition-app/static/js/main.dac7f665.chunk.js"
+  },
+  {
+    "revision": "6e7276e892c2ca01b421",
+    "url": "/face-recognition-app/static/js/runtime-main.54aca92f.js"
+  }
+]);
